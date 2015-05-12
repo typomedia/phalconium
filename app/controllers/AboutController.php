d9d76b1ed47308c24ed023412429ce9f264fe607
Philipp Speck <info@typomedia.org>
2015-05-12 21:07:51 +0200

    controller bug fixed

--- app/controllers/AboutController.php
+++ app/controllers/AboutController.php
@@ -5,9 +5,7 @@ class AboutController extends ControllerBase
 
     public function indexAction()
     {
- $robot = posts::findFirst(1);
-        echo "The robot name is ", $robot->name, "\n";
-        echo '<h1>Testtest</h1>';
+      
     }
 
 }
