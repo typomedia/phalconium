d51d2bdc7dffa90318e8f11e61c29cbf6bb7f37a
Philipp Speck <info@typomedia.org>
2015-05-14 14:53:19 +0200

    structure changed

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -10,6 +10,20 @@ class IndexController extends ControllerBase
     
     $claim = Option::findFirstByName("claim");
     $this->view->setVar("claim", $claim->getValue());
+
+    // CSS in the header
+    $this->assets
+            ->collection('header')
+            ->addCss('css/foundation/foundation.css')
+            ->addCss('css/foundation/foundation-icons.css')
+            ->addCss('css/typography.css')
+            ->addCss('css/phalconium.css');
+
+    // JS in the footer
+    $this->assets
+            ->collection('footer')
+            ->addJs('js/foundation/vendor/jquery.js')
+            ->addJs('js/foundation/foundation.min.js');
   }
 
 }
