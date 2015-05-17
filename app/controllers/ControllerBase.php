98660f31278219afbe27cb386aa91184d5839d2b
Philipp Speck <info@typomedia.org>
2015-05-17 23:47:08 +0200

    some changes added

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -4,4 +4,9 @@ use Phalcon\Mvc\Controller;
 
 class ControllerBase extends Controller {
 
+    protected function initialize()
+    {
+        //$this->tag->prependTitle('Phalconium | ');
+    }
+  
 }
